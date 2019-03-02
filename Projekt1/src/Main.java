568e7c47dddd64cf1f6d90578e398c95d1501615
7Agnieszka <45576009+7Agnieszka@users.noreply.github.com>
2019-03-02 14:57:16 +0100

    reczny commit

--- Projekt1/src/Main.java
+++ Projekt1/src/Main.java
@@ -111,8 +111,8 @@ a a^2 a^3
         System.out.println("**** Zadanie 2 **** ");
         final double pi = 3.14159;
         double r = 5;
-        System.out.printf("obwod: %f \n" , 2 * pi * r);
-        System.out.printf("pole:  %f \n" , pi * r * r);
+        System.out.printf("obwod: %f \n", 2 * pi * r);
+        System.out.printf("pole:  %f \n", pi * r * r);
 
 
 
@@ -128,13 +128,13 @@ a a^2 a^3
         int dni = ((seconds - sekundy - 60 * minuty - 3600 * godziny) / 24);
         System.out.println(dni);
 
-        System.out.println("w " + seconds + " sekund jest " +
-        seconds/60/60/24 + " dni, " + seconds%(60*60*24)/(60*60) + " godzin, " +
-                seconds%(60*60)/60 + " minut i " + seconds%60 + " sekund");
+        System.out.println("w " + seconds + " sekund jest  " +
+                seconds / 60 / 60 / 24 + " dni, " + seconds % (60 * 60 * 24) / (60 * 60) + " godzin, " +
+                seconds % (60 * 60) / 60 + " minut i " + seconds % 60 + " sekund");
 
-        System.out.println("w " + seconds + " sekund jest " + seconds/60 + " minut");
-        System.out.println("w " + seconds + " sekund jest " + seconds/3600 + " godzin");
-        System.out.println("w " + seconds + " sekund jest " + (seconds/3600)/24 + " dni");
+        System.out.println("w " + seconds + " sekund jest " + seconds / 60 + " minut");
+        System.out.println("w " + seconds + " sekund jest " + seconds / 3600 + " godzin");
+        System.out.println("w " + seconds + " sekund jest " + (seconds / 3600) / 24 + " dni");
 
 
 /* 4. Napisz program, w którym zdeniujesz dwie zmienne caªkowite i dwie zmienne zmiennoprzecinkowe
@@ -144,12 +144,12 @@ a a^2 a^3
  Liczby caªkowitej przez zmiennoprzecinkow¡
  Liczby zmiennoprzecinkowej przez liczb¦ caªkowit¡*/
         System.out.println("**** Zadanie 4 **** ");
-            int c1 =2 , c2 = 5;
-            double d1 = 2.25, d2 = -4.51;
-        System.out.println("int/double " + c1/d1);
-        System.out.println("double / int " + d2/c2);
-        System.out.println("int/int " + c2/c1);
-        System.out.println("double/double " + d2/d1);
+        int c1 = 2, c2 = 5;
+        double d1 = 2.25, d2 = -4.51;
+        System.out.println("int/double " + c1 / d1);
+        System.out.println("double / int " + d2 / c2);
+        System.out.println("int/int " + c2 / c1);
+        System.out.println("double/double " + d2 / d1);
 
 
 
@@ -168,50 +168,97 @@ a a^2 a^3
 /***********************/
 
 
+        Scanner scanner = new Scanner(System.in);
+        //String name = scanner.next();
+        String name = "Aga";
+        System.out.println("Hello " + name + " !");
 
-                Scanner scanner = new Scanner(System.in);
-                String name = scanner.next();
-                System.out.println("Hello " + name + " !");
 
-
-
-        String promien = scanner.next();
-        double pr = Double.parseDouble(promien);
-        System.out.printf("obwod: %f \n" , 2 * pi * pr);
-        System.out.printf("pole:  %f \n" , pi * pr * pr);
+        // String promien = scanner.next();
+        //double pr = Double.parseDouble(promien);
+        double pr = 4.5;
+        System.out.printf("obwod: %f \n", 2 * pi * pr);
+        System.out.printf("pole:  %f \n", pi * pr * pr);
 
         System.out.println("\n\n");
 
         System.out.println("Podaj kwote");
-        String kwotaPodana = scanner.next();
-        double calaKwota=Double.parseDouble(kwotaPodana);
+        //String kwotaPodana = scanner.next();
+        //double calaKwota=Double.parseDouble(kwotaPodana);
+        double calaKwota = 54.45;
         int kwota = (int) calaKwota;
-        System.out.println("pieciozloatowki: " + kwota/5);
-        System.out.println("dwozlotowki: " + (kwota%5)/2);
-        System.out.println("zlotowki: " + ((kwota%5)%2));
+        System.out.println("pieciozloatowki: " + kwota / 5);
+        System.out.println("dwozlotowki: " + (kwota % 5) / 2);
+        System.out.println("zlotowki: " + ((kwota % 5) % 2));
         double grosze1 = calaKwota - (double) kwota;
-        int grosze = (int) (grosze1*100);
-        System.out.println("piedziesiat groszy: " + grosze/50);
-        System.out.println("dwadziescia groszy: " + (grosze%50)/20);
-        System.out.println("dziesiec groszy: " + ((grosze%50)%20)/10);
-        System.out.println("piec groszy: " + (grosze%10)/5);
-        System.out.println("dwa grosze: " + (grosze%5)/2);
-        System.out.println("grosz: " + ((grosze%5)%2));
+        int grosze = (int) (grosze1 * 100);
+        System.out.println("piedziesiat groszy: " + grosze / 50);
+        System.out.println("dwadziescia groszy: " + (grosze % 50) / 20);
+        System.out.println("dziesiec groszy: " + ((grosze % 50) % 20) / 10);
+        System.out.println("piec groszy: " + (grosze % 10) / 5);
+        System.out.println("dwa grosze: " + (grosze % 5) / 2);
+        System.out.println("grosz: " + ((grosze % 5) % 2));
 
 
         Random rr = new Random();
-        int aaa = (rr.nextInt())%16+35;
-        double bbb = (1.5*rr.nextDouble()+1.4);
+        int aaa = (rr.nextInt(100)) % 16 + 35;
+        double bbb = (1.5 * rr.nextDouble() + 1.4);
 
         System.out.println(aaa);
         System.out.println(bbb);
+        System.out.println(rr.nextGaussian());
 
 
+        double prK = Double.parseDouble(scanner.next());
+        if (prK <= 0) {
+            System.out.println("liczba musi byc dodatnia");
+        } else {
+            System.out.printf("obwod: %f \n", 2 * pi * prK);
+            System.out.printf("pole:  %f \n", pi * prK * prK);
+        }
+
+        int liczba1 = scanner.nextInt();
+        int liczba2 = scanner.nextInt();
+        if (liczba1 > liczba2) System.out.println("wieksza " + liczba1);
+        else if (liczba1 < liczba2) System.out.println("wieksza " + liczba2);
+        else System.out.println("Rowne");
+
+
+        int procenty = scanner.nextInt();
+        if (procenty < 50) System.out.println("Ocena: 2");
+        else if (procenty >= 50 && procenty < 60) System.out.println("Ocena: 3");
+        else if (procenty >= 60 && procenty < 70) System.out.println("Ocena: 3+");
+        else if (procenty >= 70 && procenty < 80) System.out.println("Ocena: 4");
+        else if (procenty >= 80 && procenty < 90) System.out.println("Ocena: 4+");
+        else if (procenty >= 90 && procenty < 100) System.out.println("Ocena: 5");
+        else System.out.println("niepoprawne dane");
+
+
+        int rok = scanner.nextInt();
+        if (rok > 0) {
+            if (rok % 4 == 0 && rok % 100 != 0) System.out.println("Przestepny");
+            else System.out.println("nie przestepny");
+        } else System.out.println("niepprawny rok");
+
+
+        int dzien = rr.nextInt(6);
+        switch (dzien)
+        {
+            case 0: System.out.println("niedziela"); break;
+            case 1: System.out.println("poniedzialek"); break;
+            case 2: System.out.println("wtorek"); break;
+            case 3: System.out.println("sroda"); break;
+            case 4: System.out.println("czwartek"); break;
+            case 5: System.out.println("piatek"); break;
+            case 6: System.out.println("sobota"); break;
+            default:
+                System.out.println("inny");
+        }
+        int rokLosowy = rr.nextInt(1000)+1000;
+        String text =
+        ((rokLosowy % 4 == 0 && rokLosowy % 100 != 0) ? "przestepny" : "nie przestepny");
+        System.out.println(text);
     }
-
-
-
 }
-
 
 
